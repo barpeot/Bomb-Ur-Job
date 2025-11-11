bc92485ca99fdca7252c3ebc62d2d29a3f072fa7
barpeot <akbar.putra02.ap@gmail.com>
2025-11-11 22:36:43 +0700

    add simple menu

---
+++ Assets/Scripts/Misc/MainMenuController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject tutorialMenu;
+    public GameObject titleMenu;
+
+    public string gameScene;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+    
+    public void OpenTutorial()
+    {
+        tutorialMenu.SetActive(true);
+        titleMenu.SetActive(false);
+    }
+
+    public void CloseTutorial()
+    {
+        tutorialMenu.SetActive(false);
+        titleMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
